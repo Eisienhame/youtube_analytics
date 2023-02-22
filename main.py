76591596a2a9b3be82ec3c31d6eedae30fc1c7e8
Eisiehame <knurov24@gmail.com>
2023-02-22 19:00:02 +0200

    lesson 13.4 - f

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from utils import Ychannel
 
 
 vdud = Ychannel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-
+red = Ychannel('UC1eFXmJNkjITxPFWTy6RsWg')
 print(vdud.title)
 print(vdud.videocount)
 print(vdud.url)
 
 print(Ychannel.get_service())
+
+print(vdud > red)
