bff0e0c5563e575a2d12f1313a745c3c69b0f04c
Eisiehame <knurov24@gmail.com>
2023-03-05 19:35:57 +0200

    lesson 15.2-2 - start

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 
 from googleapiclient.discovery import build
-from utils import Ychannel, Video, PLVideo
+from utils.ychannel import Ychannel
+from utils.video import Video, PLVideo
+from utils.playlist import PlayList
 
 
 
@@ -10,5 +12,7 @@ red = Ychannel('UC1eFXmJNkjITxPFWTy6RsWg')
 
 video1 = Video('9lO06Zxhu88')
 video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-print(video1)
-print(video2)
+pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+print(pl.playlist_name)
+print(pl.url)
+print(pl.playlist_dict2())
